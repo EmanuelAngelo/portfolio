2a6dbd8917f8cf243a0ba9441c3e5d8a333fa4c4
Emanuel Angelo <emanuelangelo@outlook.com.br>
2024-08-05 15:57:15 -0300

    att

--- components.d.ts
+++ components.d.ts
@@ -10,7 +10,6 @@ declare module 'vue' {
     Contato: typeof import('./src/components/Contato.vue')['default']
     ConteudoPrincipal: typeof import('./src/components/ConteudoPrincipal.vue')['default']
     Empregos: typeof import('./src/components/Empregos.vue')['default']
-    HelloWorld: typeof import('./src/components/HelloWorld.vue')['default']
     MeusProjetos: typeof import('./src/components/MeusProjetos.vue')['default']
     MinhasSkill: typeof import('./src/components/MinhasSkill.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
--- src/components/ConteudoPrincipal.vue
+++ src/components/ConteudoPrincipal.vue
@@ -1,30 +1,55 @@
 <template>
   <v-app id="inspire">
-    <v-navigation-drawer v-model="drawer" app :width="270" class="pt-2 image-bg">
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+      :width="270"
+      class="pt-2 image-bg"
+    >
       <v-list>
-        <v-list-item prepend-avatar="https://avatars.githubusercontent.com/u/23459929?v=4s" title="Emanuel Angelo">
-          <v-list-item-subtitle style="font-size: 10px;">emanuelangelo@outlook.com.br</v-list-item-subtitle>
+        <v-list-item
+          prepend-avatar="https://avatars.githubusercontent.com/u/23459929?v=4s"
+          title="Emanuel Angelo"
+        >
+          <v-list-item-subtitle style="font-size: 10px"
+            >emanuelangelo@outlook.com.br</v-list-item-subtitle
+          >
         </v-list-item>
       </v-list>
-
 
       <v-divider></v-divider>
 
       <v-list dense>
-        <v-list-item prepend-icon="mdi-account-arrow-right" title="Sobre" value="sobre"
-          @click="setView('sobre')"></v-list-item>
-        <v-list-item prepend-icon="mdi-fingerprint" title="Minhas Skill" value="skills"
-          @click="setView('skills')"></v-list-item>
-        <v-list-item prepend-icon="mdi-github" title="Meus Projetos" value="projetos"
-          @click="setView('projetos')"></v-list-item>
-        <v-list-item prepend-icon="mdi-email-multiple" title="Contate-Me" value="contate"
-          @click="setView('contate')"></v-list-item>
+        <v-list-item
+          prepend-icon="mdi-account-arrow-right"
+          title="Sobre"
+          value="sobre"
+          @click="setView('sobre')"
+        ></v-list-item>
+        <!-- <v-list-item prepend-icon="mdi-fingerprint" title="Minhas Skill" value="skills"
+          @click="setView('skills')"></v-list-item> -->
+        <v-list-item
+          prepend-icon="mdi-github"
+          title="Meus Projetos"
+          value="projetos"
+          @click="setView('projetos')"
+        ></v-list-item>
+        <v-list-item
+          prepend-icon="mdi-email-multiple"
+          title="Contate-Me"
+          value="contate"
+          @click="setView('contate')"
+        ></v-list-item>
       </v-list>
     </v-navigation-drawer>
 
     <v-app-bar app dark class="custom-app-bar">
       <v-spacer></v-spacer>
-      <v-list-item @click="drawer = !drawer" prepend-icon="mdi-menu-open" :title="currentViewTitle"></v-list-item>
+      <v-list-item
+        @click="drawer = !drawer"
+        prepend-icon="mdi-menu-open"
+        :title="currentViewTitle"
+      ></v-list-item>
     </v-app-bar>
 
     <v-main class="fundo-back-mmain">
@@ -37,11 +62,11 @@
 </template>
 
 <script lang="ts">
-import { computed, defineComponent, ref } from 'vue';
-import Contato from './Contato.vue';
-import SobreMim from './SobreMim.vue';
-import MinhasSkill from './MinhasSkill.vue';
-import MeusProjetos from './MeusProjetos.vue';
+import { computed, defineComponent, ref } from "vue";
+import Contato from "./Contato.vue";
+import SobreMim from "./SobreMim.vue";
+import MinhasSkill from "./MinhasSkill.vue";
+import MeusProjetos from "./MeusProjetos.vue";
 
 export default defineComponent({
   components: {
@@ -52,28 +77,28 @@ export default defineComponent({
   },
   setup() {
     const drawer = ref(false);
-    const currentView = ref('sobre'); // estado inicial
+    const currentView = ref("sobre"); // estado inicial
 
     const setView = (view) => {
       currentView.value = view;
     };
 
     const viewTitles = {
-      sobre: 'Sobre',
-      skills: 'Skill',
-      projetos: 'Projetos',
-      contate: 'Contato'
+      sobre: "Sobre",
+      skills: "Skill",
+      projetos: "Projetos",
+      contate: "Contato",
     };
 
     const currentViewTitle = computed(() => {
-      return viewTitles[currentView.value] || 'Menu';
+      return viewTitles[currentView.value] || "Menu";
     });
 
     return {
       drawer,
       currentView,
       setView,
-      currentViewTitle
+      currentViewTitle,
     };
   },
 });
