a50e61fffc365b763b456e9d08b1ab3b50d6bd6f
Emanuel Almeida <emanuelangelo@outlook.com.br>
2025-01-14 19:59:01 -0300

    ajustando drawe

--- src/components/PrincipalBase.vue
+++ src/components/PrincipalBase.vue
@@ -7,6 +7,7 @@
         permanent
         @click="rail = false"
         class="image-bg"
+        :style="{ zIndex: 1000 }"
       >
         <v-list-item
           prepend-avatar="https://avatars.githubusercontent.com/u/23459929?v=4"
@@ -47,7 +48,10 @@
       </v-navigation-drawer>
 
       <!-- Área principal onde os componentes são renderizados -->
-      <v-main class="fundo-back-mmain" style="min-height: 100vh; flex-grow: 1">
+      <v-main
+        class="fundo-back-mmain"
+        style="min-height: 100vh; flex-grow: 1; z-index: 500"
+      >
         <component :is="activeComponent" />
       </v-main>
     </v-layout>
@@ -82,10 +86,22 @@ export default {
 .image-bg {
   background-color: #364349;
   background-size: cover;
+  transition: transform 0.3s ease;
 }
 
 .fundo-back-mmain {
   background-color: #1b3e4e;
   background-size: cover;
+  position: relative;
+  z-index: 500;
+}
+
+.v-navigation-drawer--permanent {
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  width: 250px; /* Largura do drawer */
+  z-index: 1000 !important; /* Elevando o drawer sobre o conteúdo */
 }
 </style>
